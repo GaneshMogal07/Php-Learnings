7328e747503ee0d8ecc74d32c8d8e9f575b7b568
GaneshMogal07 <ganeshmogal35@gmail.com>
2025-09-03 10:51:49 +0530

    test by ganesh

--- Php Pratice/PHP Syntax/php_tags.php
+++ Php Pratice/PHP Syntax/php_tags.php
@@ -1,8 +1,12 @@
 <?php
 echo "Hello, World!";
 ?>
+
 <?php // tells the server to start PHP code
 ?> // tells the server to end PHP code
 
 echo // prints output to the browser
 // output - Hello, World!
+
+
+replict app.                                                                                                                                                                                                                                                                                                                                                  
